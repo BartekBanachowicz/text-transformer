cfbed5db715bac6fce6f2ce7a60332732d8651e8
Jakub Błażejowski <56088851+Jorewin@users.noreply.github.com>
2021-12-20 09:55:24 +0100

    Update ToUpper transformation

--- src/main/java/pl/put/poznan/transformer/logic/ToUpper.java
+++ src/main/java/pl/put/poznan/transformer/logic/ToUpper.java
@@ -3,15 +3,11 @@ package pl.put.poznan.transformer.logic;
 import java.util.Locale;
 
 public class ToUpper extends TextDecorator {
-    private Transformer transformer;
 
-    public ToUpper(Transformer t) {
-        super(t);
-        this.transformer = t;
-    }
+    public ToUpper(Transformer t) { super(t); }
 
     @Override
     public String GetText() {
-        return super.GetText().toUpperCase(Locale.ROOT);
+        return super.GetText().toUpperCase(Locale.ENGLISH);
     }
 }
